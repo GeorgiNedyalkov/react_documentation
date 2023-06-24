b540409ab7126b9b9e0035166a5cbb51e21d95df
Flu3nt <gnedyalkov94@gmail.com>
2023-06-24 19:09:14 +0300

    add tailwind to project

--- state_management/typescript_state_management/src/App.tsx
+++ state_management/typescript_state_management/src/App.tsx
@@ -1,13 +1,57 @@
-import { useState } from "react";
+import { useState, useEffect, createContext, useContext } from "react";
 
-function usePokemon() {
-  const [pokemon] = useState([]);
+interface Pokemon {
+  id: number;
+  name: string;
+  type: string[];
+  hp: number;
+  attack: number;
+  defense: number;
+  special_attack: number;
+  special_defense: number;
+  speed: number;
+}
+
+function usePokemonSource(): {
+  pokemon: Pokemon[];
+} {
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    fetch("/pokemon.json")
+      .then((res) => res.json())
+      .then((data) => setPokemon(data));
+  }, []);
 
   return { pokemon };
 }
 
+function usePokemon() {
+  return useContext(PokemonContext);
+}
+
+function PokemonList() {
+  const { pokemon } = usePokemon();
+
+  return (
+    <div>
+      {pokemon.map((p) => (
+        <div key={p.id}>{p.name}</div>
+      ))}
+    </div>
+  );
+}
+
+const PokemonContext = createContext<ReturnType<typeof usePokemonSource>>(
+  {} as unknown as ReturnType<typeof usePokemonSource>
+);
+
 function App() {
-  return <div></div>;
+  return (
+    <PokemonContext.Provider value={usePokemonSource()}>
+      <PokemonList />
+    </PokemonContext.Provider>
+  );
 }
 
 export default App;
