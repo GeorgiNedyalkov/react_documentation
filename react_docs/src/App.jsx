d59f6d497046dfa1bd56bab3de9fa32e32c53bb8
Flu3nt <gnedyalkov94@gmail.com>
2023-07-02 14:11:56 +0300

    remove input focus from app

--- react_docs/src/App.jsx
+++ react_docs/src/App.jsx
@@ -1,20 +1,3 @@
-import { useRef } from "react";
-
 export default function App() {
   return <div className="m-10"></div>;
-}
-
-function InputFocus() {
-  const myRef = useRef(null);
-
-  function handleFocus() {
-    myRef.current.focus();
-  }
-
-  return (
-    <>
-      <input ref={myRef} type="text" placeholder="Hi" />
-      <button onClick={handleFocus}>Focus</button>
-    </>
-  );
 }
