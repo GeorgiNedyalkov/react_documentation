33613c16428c1446d159b22737095770595911a8
GeorgiNedyalkov <gnedyalkov94@gmail.com>
2023-07-07 12:31:41 +0300

    start working on custom hooks

--- react_docs/src/App.jsx
+++ react_docs/src/App.jsx
@@ -1,3 +1,29 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
   return <div className="m-10"></div>;
+}
+
+export function StatusBar() {
+  const [isOnline, setIsOnline] = useState(false);
+
+  useEffect(() => {
+    function handleOnline() {
+      setIsOnline(true);
+    }
+
+    function handleOffline() {
+      setIsOnline(false);
+    }
+
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, []);
+
+  return <h1>{isOnline ? "✅ Success" : "❌ Offline"}</h1>;
 }
